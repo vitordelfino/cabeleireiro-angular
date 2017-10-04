1532cddd4ae65ff1922127c585143b152cedcf82
vitordelfino <vitor.delfino952@gmail.com>
2017-10-04 18:16:19 -0300

    servico

--- src/app/servico/servico.service.ts
+++ src/app/servico/servico.service.ts
@@ -7,8 +7,7 @@ export class ServicoService {
   constructor(private _http: Http) { }
 
   find(){
-    //https://cabeleireiro-api.herokuapp.com/servicos/find'
-    //https://cabeleireiro-api.herokuapp.com/servicos/find'
+
     return this._http
       .get('https://cabeleireiro-api.herokuapp.com/servicos/find')
       .map(res => res.json())
